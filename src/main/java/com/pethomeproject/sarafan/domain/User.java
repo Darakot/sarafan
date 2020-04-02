d3ff4bc4e67ddcd4c3d4eac17b8d1f021310f8de
darakot <dmitriy0305@yandex.ru>
2020-04-02 18:08:44 +0500

    Added Spring Security oAuth2

---
+++ src/main/java/com/pethomeproject/sarafan/domain/User.java
@@ -0,0 +1,25 @@
+package com.pethomeproject.sarafan.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "usr")
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String userpic;
+    private String email;
+    private String gender;
+    private String locale;
+    private LocalDateTime lastVisit;
+}
